5d5f5ca065cf6a373e5b2a758cb517f400247876
zmf <1094178787@qq.com>
2022-08-16 22:13:28 +0800

    二叉搜索树

---
+++ tree/450.删除二叉搜索树中的节点.js
@@ -0,0 +1,125 @@
+/*
+ * @lc app=leetcode.cn id=450 lang=javascript
+ *
+ * [450] 删除二叉搜索树中的节点
+ *
+ * https://leetcode.cn/problems/delete-node-in-a-bst/description/
+ *
+ * algorithms
+ * Medium (52.25%)
+ * Likes:    931
+ * Dislikes: 0
+ * Total Accepted:    154.3K
+ * Total Submissions: 295.1K
+ * Testcase Example:  '[5,3,6,2,4,null,7]\n3'
+ *
+ * 给定一个二叉搜索树的根节点 root 和一个值 key，删除二叉搜索树中的 key
+ * 对应的节点，并保证二叉搜索树的性质不变。返回二叉搜索树（有可能被更新）的根节点的引用。
+ * 
+ * 一般来说，删除节点可分为两个步骤：
+ * 
+ * 
+ * 首先找到需要删除的节点；
+ * 如果找到了，删除它。
+ * 
+ * 
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 
+ * 
+ * 
+ * 输入：root = [5,3,6,2,4,null,7], key = 3
+ * 输出：[5,4,6,2,null,null,7]
+ * 解释：给定需要删除的节点值是 3，所以我们首先找到 3 这个节点，然后删除它。
+ * 一个正确的答案是 [5,4,6,2,null,null,7], 如下图所示。
+ * 另一个正确答案是 [5,2,6,null,4,null,7]。
+ * 
+ * 
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 
+ * 输入: root = [5,3,6,2,4,null,7], key = 0
+ * 输出: [5,3,6,2,4,null,7]
+ * 解释: 二叉树不包含值为 0 的节点
+ * 
+ * 
+ * 示例 3:
+ * 
+ * 
+ * 输入: root = [], key = 0
+ * 输出: []
+ * 
+ * 
+ * 
+ * 提示:
+ * 
+ * 
+ * 节点数的范围 [0, 10^4].
+ * -10^5 <= Node.val <= 10^5
+ * 节点值唯一
+ * root 是合法的二叉搜索树
+ * -10^5 <= key <= 10^5
+ * 
+ * 
+ * 
+ * 
+ * 进阶： 要求算法时间复杂度为 O(h)，h 为树的高度。
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} key
+ * @return {TreeNode}
+ */
+var deleteNode = function (root, key) {
+    if (!root) {
+        return null;
+    }
+    if (root.val == key) {
+        //删除结点
+        if (root.left == null && root.right == null) {
+            return null;
+        }
+        if (root.left == null) {
+            return root.right
+        }
+        if (root.right == null) {
+            return root.left
+        }
+        var min = getMin(root.right);
+        root.right = deleteNode(root.right, min.val);
+        min.left = root.left;
+        min.right = root.right;
+        root = min;
+    }
+    if (root.val > key) {
+        root.left = deleteNode(root.left, key);
+    }
+    if (root.val < key) {
+        root.right = deleteNode(root.right, key);
+    }
+    return root;
+};
+
+function getMin(node) {
+    while (node.left) {
+        node = node.left;
+    }
+    return node;
+}
+// @lc code=end
+
