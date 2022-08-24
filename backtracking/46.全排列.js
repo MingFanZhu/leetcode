b8613f1dfbb5d341cca6032869ad0fd552ecb173
zmf <1094178787@qq.com>
2022-08-24 22:23:34 +0800

    回溯算法

---
+++ backtracking/46.全排列.js
@@ -0,0 +1,79 @@
+/*
+ * @lc app=leetcode.cn id=46 lang=javascript
+ *
+ * [46] 全排列
+ *
+ * https://leetcode.cn/problems/permutations/description/
+ *
+ * algorithms
+ * Medium (78.69%)
+ * Likes:    2185
+ * Dislikes: 0
+ * Total Accepted:    703.3K
+ * Total Submissions: 893.4K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * 给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：nums = [1,2,3]
+ * 输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：nums = [0,1]
+ * 输出：[[0,1],[1,0]]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：nums = [1]
+ * 输出：[[1]]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= nums.length <= 6
+ * -10 <= nums[i] <= 10
+ * nums 中的所有整数 互不相同
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function(nums) {
+    var res=[];
+    var path=[];
+    function travel(){
+        if(path.length==nums.length){
+            res.push(path.map((val)=>val));
+            return;
+        }
+        for(var i of nums){
+            if(path.indexOf(i)!=-1){
+                continue;
+            }
+            path.push(i);
+            travel();
+            path.pop();
+        }
+    }
+    travel();
+    return res;
+};
+// @lc code=end
+
